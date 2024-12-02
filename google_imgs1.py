9978cc585b358922c4bff1df92b9f5b056120a68
aravinda_gn <aravinda.gn123@gmail.com>
2024-12-02 23:58:12 +0530

    Change google_imgs1.py

--- google_imgs1.py
+++ google_imgs1.py
@@ -18,12 +18,18 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('webdriver//chromedriver',chrome_options=chrome_options)# Create url variable containing the webpage for a Google image search.
 
 url = ("https://www.google.com/search?q={s}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568")# Launch the browser and open the given url in the webdriver.
+# url = ("https://www.google.com/search?q={s}&tbm=isch")
 
-driver.get(url.format(s='dogs'))# Scroll down the body of the web page and load the images.
+driver.get(url.format(s='car'))# Scroll down the body of the web page and load the images.
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
 time.sleep(5)# Find the images.
 
-imgResults = driver.find_elements(By.XPATH,"//img[contains(@class,'YQ4gaf')]")# Access and store the scr list of image url's.
+# imgResults = driver.find_elements(By.XPATH,"//img[contains(@class,'YQ4gaf')]")# Access and store the scr list of image url's.
+# imgResults = driver.find_elements(By.XPATH,"//img[contains(@class,'sFlh5c FyHeAf')]")# Access and store the scr list of image url's.
+
+# imgResults = driver.find_elements(By.XPATH,"//img[contains(@class,'YQ4gaf')]")# Access and store the scr list of image url's.
+# imgResults = driver.find_elements(By.XPATH,"//img[contains(@id,'dimg_N9hNZ6KoNIub4-EP94O6iAI')]")# Access and store the scr list of image url's.
+imgResults = driver.find_elements(By.XPATH,"//img[contains(@class, 'sFlh5c FyHeAf iPVvYb')]")
 
 src = []
 for img in imgResults:
